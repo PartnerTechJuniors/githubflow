361e0aa4be4f596c344a1716459055f22a334400
Kevin Arnold Flores Pacheco <72578517+kev1n01@users.noreply.github.com>
2025-11-11 13:40:32 -0500

    Feature/agregar perfil usuario (#3)
    * generando componente para vista/pagina perfil de usuario
    
    * maquetado de html listo

---
+++ src/app/pages/profileusers/profileusers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profileusers',
+  imports: [],
+  templateUrl: './profileusers.html',
+  styleUrl: './profileusers.css',
+})
+export class Profileusers {
+
+}
