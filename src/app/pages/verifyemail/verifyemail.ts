aefc4cf436b304cd70a6a19dd377a5b16af28631
Kevin Arnold Flores Pacheco <72578517+kev1n01@users.noreply.github.com>
2025-11-11 14:15:35 -0500

    generando vista para verificar email (#4)

---
+++ src/app/pages/verifyemail/verifyemail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifyemail',
+  imports: [],
+  templateUrl: './verifyemail.html',
+  styleUrl: './verifyemail.css',
+})
+export class Verifyemail {
+
+}
